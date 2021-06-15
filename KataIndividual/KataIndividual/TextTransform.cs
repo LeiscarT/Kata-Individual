0324b8cf32a7bd1da517d04722fed7c44ecd1e80
LeiscarT <leiscartp29@gmail.com>
2021-06-15 12:01:58 -0400

    Initial Commit

---
+++ KataIndividual/KataIndividual/TextTransform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataIndividual
+{
+    public class TextTransform
+    {
+
+        public string convertToCame(string text)
+        {
+            return text;
+        }
+    }
+}
---
+++ KataIndividual/TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using KataIndividual;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+           
+            Assert.Pass();
+        }
+    }
+}
